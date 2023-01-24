4d555c1aeedaa7372e0ca60afbb5fc3eecf81d0f
jojonoskill <kostamatusenko@gmail.com>
2023-01-24 20:13:48 +0100

    Use camelCase

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,9 +1,9 @@
 'use strict';
 // Return an array without duplicates
 
-const duplicate = (value, N) => {
+const duplicate = (value, n) => {
   const result = [];
-  for (let i = 0; i < N; i++) {
+  for (let i = 0; i < n; i++) {
     result.push(value);
   }
   return result;
